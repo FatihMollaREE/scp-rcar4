836f3510dfd5d99a4dedc372559432a34264fc18
Nancy . <nancy@arm.com>
2024-06-19 14:21:23 +0530

    rdv3r1: add sid module config data for scpfw
    The SID peripheral provides information about the platform and its
    configuration. So, add the configuration data for sid module in scp
    ramfw. The configuration data includes the expected value of its
    PID/CID registers for identification. PID5, PID6 and PID7 are not
    implemented by this peripheral and this is indicated using the valid
    register bitmap.
    
    Signed-off-by: Nancy . <nancy@arm.com>
    Change-Id: Id44cd8f06b13437651e8d7cadeca0b9edf77f82c

---
+++ product/neoverse-rd/rdv3r1/scp_ramfw/include/scp_css_mmap.h
@@ -0,0 +1,22 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Base address definitions for the SCP's sub-system and access extending
+ *     into the rest of the CSS.
+ */
+
+#ifndef SCP_CSS_MMAP_H
+#define SCP_CSS_MMAP_H
+
+// clang-format off
+
+/* SCP sub-system peripherals */
+#define SCP_SID_BASE (0x1A4A0000)
+
+// clang-format on
+
+#endif /* SCP_CSS_MMAP_H */
