e851864be7c958c55d5bc63f8f09d74a40a6d625
Nancy . <nancy@arm.com>
2024-11-21 14:24:04 +0530

    rdv3r1: add config data for pl011 UART module in lcpfw
    PL011 controller is used as a console port for debug and log messages.
    Add configuration data of this controller including base address and
    input clock frequency for the PL011 module to use.
    
    Signed-off-by: Nancy . <nancy@arm.com>
    Signed-off-by: Shriram K <shriram.k@arm.com>
    Change-Id: I13707d9aa2393620c69cc352dbd8646309689e6c

---
+++ product/neoverse-rd/rdv3r1/lcp_ramfw/include/lcp_css_mmap.h
@@ -0,0 +1,23 @@
+/*
+ * Arm SCP/MCP Software
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Description:
+ *     Base address definitions for the LCP's sub-system.
+ */
+
+#ifndef LCP_CSS_MMAP_H
+#define LCP_CSS_MMAP_H
+
+#include <fwk_macros.h>
+
+// clang-format off
+
+/* LCP sub-system peripherals */
+#define LCP_UART_BASE (0xB5080000)
+
+// clang-format on
+
+#endif /* LCP_CSS_MMAP_H */
