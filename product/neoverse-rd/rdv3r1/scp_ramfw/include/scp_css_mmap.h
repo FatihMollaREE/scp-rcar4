50aedc615d3241e12daf94882ef54612d24664d1
Nancy . <nancy@arm.com>
2024-11-26 07:06:55 +0000

    rdv3r1: add definitions for address translation windows 0 and 1
    There are two address translation windows that allow SCP firmware to map
    regions of other subsystem memory space into SCP's address space. The
    size of both of these translation windows is 1GB. All accesses through
    the address translation windows are further mapped via the ATU and so
    all the addresses accessed through the address translation windows are
    treated as logical addresses which are then mapped to physical address
    by the ATU.
    
    Address translation window 0 (ATW0) is used for mapping the cluster
    utility MMAP region in the AP address map. The cluster utility region
    address space includes two addressable regions - the AP cluster
    peripherals and the LCP peripherals. The AP cluster peripherals are
    mapped in the AP address map at 0x140000000. The LCP peripherals are
    mapped in the AP address map at 0x160000000.
    
    Address translation window 1 (ATW1) is used for mapping the CMN
    peripheral address region.
    
    Add initial definitions of ATW0 and ATW1 and all the mapping that are
    addressable through it.
    
    Signed-off-by: Nancy . <nancy@arm.com>
    Change-Id: Ic2a44779135dccb73b7a6ca91e6224eec00e7a7c

--- product/neoverse-rd/rdv3r1/scp_ramfw/include/scp_css_mmap.h
+++ product/neoverse-rd/rdv3r1/scp_ramfw/include/scp_css_mmap.h
@@ -18,6 +18,10 @@
 #define SCP_SID_BASE (0x1A4A0000)
 #define SCP_UART_BASE (0x44002000)
 
+/* SCP addresses mapped via ATU into address translation windows */
+#define SCP_ADDRESS_TRANSLATION_WINDOW0_BASE (0x60000000)
+#define SCP_ADDRESS_TRANSLATION_WINDOW1_BASE (0xA0000000)
+
 // clang-format on
 
 #endif /* SCP_CSS_MMAP_H */
